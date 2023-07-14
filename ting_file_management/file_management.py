97c4432349f58ef54636e410262d0c250d507c34
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-07-14 16:19:04 -0300

    Req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,5 +1,18 @@
-# from ting_file_management.queue import Queue
+import sys
+from ting_file_management.queue import Queue
 
 
 def txt_importer(path_file):
-    """Aqui irá sua implementação!"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+    queue = Queue()
+    txt_list = []
+    try:
+        with open(path_file, 'r') as file:
+            for line in file:
+                txt_list.append(line.rstrip("\n"))
+                queue.enqueue
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+    else:
+        return txt_list
