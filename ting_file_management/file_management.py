ece97cb02e5c5f2cb5feb9a9b9b6b8e683d0ebce
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-07-15 14:52:36 -0300

    Req 3

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,17 +1,14 @@
 import sys
-from ting_file_management.queue import Queue
 
 
 def txt_importer(path_file):
     if not path_file.endswith(".txt"):
         print("Formato inválido", file=sys.stderr)
-    queue = Queue()
     txt_list = []
     try:
         with open(path_file, 'r') as file:
             for line in file:
                 txt_list.append(line.rstrip("\n"))
-                # queue.enqueue
     except FileNotFoundError:
         print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
     else:
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    list_txt = txt_importer(path_file)
+    queue = instance
+    for i in range(len(queue)):
+        if queue.search(i)["nome_do_arquivo"] == path_file:
+            return
+    value = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(list_txt),
+        "linhas_do_arquivo": list_txt
+    }
+    queue.enqueue(value)
+    print(value)
 
 
 def remove(instance):
