485e02411c1a2a40b75df672e175b29e1f30f2b7
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-07-16 17:30:36 -0300

    Req 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -5,8 +5,8 @@ def exists_word(word, instance):
         unic_elem = queue.search(i)
         occurrences = []
         for j in range(unic_elem['qtd_linhas']):  # lista de linhas
-            linhas = unic_elem['linhas_do_arquivo'][j]
-            text_list = linhas.lower().rstrip('.').split()
+            line = unic_elem['linhas_do_arquivo'][j]
+            text_list = line.lower().rstrip('.').split()
             if word in text_list:
                 occurrences.append({'linha': j + 1})
         if occurrences:
@@ -20,4 +20,21 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    queue = instance
+    final_list = []
+    for i in range(len(queue)):
+        unic_elem = queue.search(i)
+        occurrences = []
+        for j in range(unic_elem['qtd_linhas']):  # lista de linhas
+            line = unic_elem['linhas_do_arquivo'][j]
+            text_list = line.lower().rstrip('.').split()
+            if word in text_list:
+                occurrences.append({'linha': j + 1, "conteudo": line})
+        if occurrences:
+            final_list.append({
+                "palavra": word,
+                "arquivo": unic_elem['nome_do_arquivo'],
+                "ocorrencias": occurrences
+                })
+
+    return final_list
