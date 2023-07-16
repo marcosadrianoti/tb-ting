fdf972e60ec68dfa11e40ced3740f53b20c2386e
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-07-16 11:09:23 -0300

    Req 7 - 1 passed

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementaçãoo"""
+    queue = instance
+    # {
+    #     "nome_do_arquivo": "c:/9.txt",
+    #     "linhas_do_arquivo": ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    #     "qtd_linhas": 9,
+    # }
+    final_list = []
+    for i in range(0, len(queue) - 1):  # lista principal
+        ocorrencias = []
+        for j in range(0, queue[i]['qtd_linhas'] - 1):  # lista de linhas
+            text_list = queue[i]['linhas_do_arquivo'][j].lower().split()
+            if word in text_list:
+                ocorrencias.append({'linha': j + 1})
+        if ocorrencias:
+            final_list.append({
+                "palavra": word,
+                "arquivo": queue[i]['nome_do_arquivo'],
+                "ocorrencias": ocorrencias
+                })
+
+    return final_list
 
 
 def search_by_word(word, instance):
